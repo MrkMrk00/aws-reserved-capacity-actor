8633bf6546b6619958f069a5c7d480c1fa0253f6
MrkMrk00 <mhau@outlook.cz>
2025-10-06 15:59:42 +0200

    feat: use AWS access token from input/env

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import dataclasses
 import datetime
+import os
 import pprint
 from typing import Generator
 
@@ -27,15 +28,33 @@ def get_expiring_soon(
             yield capacity
 
 
+def create_aws_session(actor_input: dict) -> boto3.Session:
+    access_key_id = actor_input.get('aws_access_key_id',
+                                    os.environ.get('AWS_ACCESS_KEY_ID'))
+    secret_access_key = actor_input.get('aws_secret_access_key',
+                                        os.environ.get('AWS_SECRET_ACCESS_KEY'))
+
+    assert access_key_id is not None and secret_access_key is not None, \
+        'AWS access token was not provided'
+
+    return boto3.Session(
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+    )
+
+
 async def main() -> None:
     async with Actor:
         input = await Actor.get_input()
+        session = create_aws_session(input)
+        client = CustomAwsClient(session, region='us-east-1')
 
-        client = CustomAwsClient(boto3.Session(), region='us-east-1')
         reserved_capacities = client.dynamodb_reserved_capacities()
+        expiring_soon = list(get_expiring_soon(
+            reserved_capacities, datetime.timedelta(days=input.get('days'))))
 
-        expiring_soon = get_expiring_soon(
-            reserved_capacities, datetime.timedelta(days=input.get('days')))
+        Actor.log.info('%d DynamoDB reserved capacities expiring soon',
+                       len(expiring_soon))
 
         pprint.pprint(list(expiring_soon))
 
