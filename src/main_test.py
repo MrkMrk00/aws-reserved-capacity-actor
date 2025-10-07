81e3ceaa61a0e8491a8c32fe98b4ef4fe711cf5d
MrkMrk00 <mhau@outlook.cz>
2025-10-07 08:41:36 +0200

    feat: add test for notification timing

---
+++ src/main_test.py
@@ -0,0 +1,44 @@
+import datetime
+import math
+import unittest
+
+from .aws import ReservedCapacity
+from .main import get_expiring_soon
+
+
+def _create_test_reserved_capacity(id: str, expire_date: datetime.datetime):
+    start_date = datetime.datetime.now() - datetime.timedelta(days=14)
+
+    return ReservedCapacity(
+        FixedPrice=2.0,
+        InstanceCount=2,
+        StartDate=str(math.trunc(start_date.timestamp() * 1000)),
+        ReservedCapacityId=id,
+        ReservedCapacityState='active',
+        UsageType='test',
+        UsagePrice=0,
+        DurationSeconds=int((expire_date - start_date).total_seconds())
+    )
+
+
+class ExpiringTimingTestCase(unittest.TestCase):
+    def test_does_not_fire_too_early(self):
+        notify_delta = datetime.timedelta(days=3)
+        should_not_notify_time = (
+            datetime.datetime.now()
+            + notify_delta
+            + datetime.timedelta(days=1)
+        )
+        should_notify_time = (
+            datetime.datetime.now()
+            + notify_delta
+            - datetime.timedelta(days=1)
+        )
+
+        expiring_soon = list(get_expiring_soon([
+            _create_test_reserved_capacity('0', should_not_notify_time),
+            _create_test_reserved_capacity('1', should_notify_time),
+        ], notify_delta))
+
+        self.assertEqual(len(expiring_soon), 1)
+        self.assertEqual(expiring_soon[0].ReservedCapacityId, '1')
